b8d0baa508bc8558eaa1e594bfddc2e1a95ea4fe
Azhar Ali <azharali1453@gmail.com>
2022-11-19 13:30:20 +0530

    commit

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -26,5 +26,5 @@ urlpatterns = [
     path('', include('home.urls')),
     
     re_path(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATICFILES_DIRS}),
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ] 
